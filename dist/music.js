b402c9ad1940002fd5fa334a1ca1b432a1b42e93
jeffrey126 <fenghe1126@outlook.com>
2021-04-08 22:01:33 +0800

    Site updated: 2021-04-08 22:01:33

--- dist/music.js
+++ dist/music.js
@@ -12,7 +12,7 @@ const ap = new APlayer({
       {
         name: '願い~あの頃のキミへ~',
         artist: '當山みれい',
-        url: 'https://music.163.com/song/media/outer/url?id=488388942.mp3',
+        url: 'https://m7.music.126.net/20210408043606/3c25c1ae5055e23d2e452369911f1b5d/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/3571730135/77b6/a3f1/3510/4ad2749f0662231416ddb964595ee834.mp3',
         cover: 'https://p1.music.126.net/kbLlBkGfEcA3RJyC5JhkDA==/18346451021830743.jpg',
       },
     ]
